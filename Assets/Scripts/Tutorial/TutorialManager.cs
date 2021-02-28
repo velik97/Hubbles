d6bd6fa0f7cc8435d37a5cc22a739cbbcd670dbe
Nikita Velikovskiy <velik.nikita@gmail.com>
2021-03-01 01:39:14 +0300

    Update project to 2020.1.3f1

--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Analytics;
 using UnityEngine.Playables;
-using UnityEngine.Timeline;
 using UnityEngine.UI;
 
 public class TutorialManager : MonoSingleton<TutorialManager>
